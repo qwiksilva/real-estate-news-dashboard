0afed9a14cc2653e81f199dbb0ce09c250cd6860
Ryan Silva <ryan.silva95@gmail.com>
2024-10-27 16:34:31 -0400

    remove venv

--- .venv/lib/python3.9/site-packages/eventregistry/Logger.py
+++
@@ -1,8 +0,0 @@
-import logging, os
-
-logger = logging.getLogger("eventregistry")
-if len(logger.handlers) == 0:
-    logger.addHandler(logging.StreamHandler())
-
-# by default just log from Warning level up
-logger.setLevel(level = os.environ.get("LOGLEVEL", "WARNING"))
